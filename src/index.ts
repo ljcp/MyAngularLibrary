862f7e2793576fae2177304253965f46a4ea169d
Luis <luis@computer.org>
2017-03-29 01:07:59 -0600

    changes

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 
 export * from './my-library.module'
 export * from './components/my-notification.component'
+export * from './components/login.service'
--- src/my-library.module.ts
+++ src/my-library.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 
 import { MyNotificationComponent }   from './components/my-notification.component';
+import { LoginService }   from './components/login.service';
+
 
 @NgModule({
     imports: [
 
     ],
     exports: [
-            MyNotificationComponent
+            MyNotificationComponent,
+            LoginService
     ],
     declarations: [
-            MyNotificationComponent
+            MyNotificationComponent,
+            LoginService
         ],
     providers: [
 
